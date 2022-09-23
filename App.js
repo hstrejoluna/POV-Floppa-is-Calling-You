ab04d60f21f25585ee72f8557f13ea52e7024480
hstrejoluna <trejolunatutoriales@gmail.com>
2022-09-22 21:18:54 -0500

    panel

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { ImageBackground, StyleSheet, Text, View } from "react-native";
 
-
-var backgroundFloppa = require('./assets/imgone.png');
+var backgroundFloppa = require("./assets/imgone.png");
 
 const App = () => (
   <View style={styles.container}>
-    <ImageBackground source={backgroundFloppa} resizeMode="cover" style={styles.image}>
-      <Text style={styles.text}>Inside</Text>
+    <ImageBackground
+      source={backgroundFloppa}
+      resizeMode='cover'
+      style={styles.image}
+    >
+      <View style={styles.panel}></View>
     </ImageBackground>
   </View>
 );
@@ -18,16 +21,13 @@ const styles = StyleSheet.create({
   },
   image: {
     flex: 1,
-    justifyContent: "space-between"
+    justifyContent: "flex-end",
   },
-  text: {
-    color: "white",
-    fontSize: 42,
-    lineHeight: 84,
-    fontWeight: "bold",
-    textAlign: "center",
-    backgroundColor: "#000000c0"
-  }
+  panel:{
+    flexDirection: "row",
+    height: 120,
+    backgroundColor: 'steelblue',
+  },
 });
 
 export default App;
